3e92bf47e90e0d5601355a0e033bf0f039c68d19
Georgios Tsoukas <georgios@dict.gr>
2020-02-24 14:20:32 +0100

    Added ncf to last.fm table.

--- lastfm.sh
+++ lastfm.sh
@@ -36,7 +36,7 @@ wget --quiet http://mtg.upf.edu/static/datasets/last.fm/lastfm-dataset-360K.tar.
 
 tar xvzf lastfm-dataset-360K.tar.gz
 
-python ${PY_DIR}/dataprep.py lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv
+python ${PY_DIR}/sampler.py lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv
 
 log_pidstad_cmd "python ${PY_DIR}/popular.py train.svm test.svm negatives.svm ranking_popular" popular_pidstat.log
 python ${PY_DIR}/parse_pidstat_file.py popular_pidstat.log
@@ -53,6 +53,8 @@ python ${PY_DIR}/parse_pidstat_file.py google_tf_pidstat.log
 log_pidstad_cmd "python ${PY_DIR}/lyst_lightfm.py train.svm test.svm negatives.svm ranking_lyst_lightfm" lyst_lightfm_pidstat.log
 python ${PY_DIR}/parse_pidstat_file.py lyst_lightfm_pidstat.log
 
+python ${PY_DIR}/ncf/ncf.py train.svm test.svm negatives.svm ranking_ncf --model=GMF --batch_size=256 --lr=0.001 --factor_num=32 --epochs=20
+
 echo "popular:"
 python ${PY_DIR}/evaluator.py train.svm test.svm ranking_popular
 echo "benferid_implicit:"
@@ -63,3 +65,5 @@ echo "google_tf:"
 python ${PY_DIR}/evaluator.py train.svm test.svm ranking_google_tf
 echo "lyst_lightfm:"
 python ${PY_DIR}/evaluator.py train.svm test.svm ranking_lyst_lightfm
+echo "ncf:"
+python ${PY_DIR}/evaluator.py train.svm test.svm ranking_ncf
